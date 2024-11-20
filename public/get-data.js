8b09c89aee3ceffec9efae63295515d2a38a42ef
decampus <mos.decampus.cs@gmail.com>
2024-11-20 13:15:49 -0300

    feat: add search parameters options

--- public/get-data.js
+++ public/get-data.js
@@ -2,7 +2,15 @@
 const form = document.getElementById('search-form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    fetch('/getdata')
+    const date = document.getElementById('date').value;
+    const station = document.getElementById('station').value;
+    const queryParams = new URLSearchParams({
+        "TYPE": "sflist",
+        "DATE": date,
+        "HOUR": "23",
+        "STATION": station.toUpperCase()
+    });
+    fetch(`/getdata?${queryParams.toString()}`)
         .then(response => response.text())
         .then(data => {
         document.getElementById('results-display').value = data;
