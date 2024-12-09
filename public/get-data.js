8d22d231faa088268d304aee00fbfd9ce6ebe3b2
decampus <mos.decampus.cs@gmail.com>
2024-12-08 23:47:01 -0300

    feat: display formatted data

--- public/get-data.js
+++ public/get-data.js
@@ -1,9 +1,15 @@
 "use strict";
+window.onload = () => {
+    document.getElementById('results-display').value = '';
+    document.getElementById('date').valueAsDate = new Date();
+};
 const form = document.getElementById('search-form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const date = document.getElementById('date').value;
     const station = document.getElementById('station').value;
+    const resultsDsiplay = document.getElementById('results-display');
+    resultsDsiplay.value = '';
     const queryParams = new URLSearchParams({
         "TYPE": "sflist",
         "DATE": date,
@@ -13,10 +19,39 @@ form.addEventListener('submit', (event) => {
     fetch(`/getdata?${queryParams.toString()}`)
         .then(response => response.text())
         .then(data => {
-        document.getElementById('results-display').value = data;
+        const cleanedData = removeHeader(data);
+        displayData(data);
     })
         .catch(error => {
         console.error('Error fetching weather data: ', error);
         document.getElementById('results-display').value = 'Failed to load data.';
     });
 });
+function getDataHeader(data) {
+    let contentArray = data.split('\n');
+    contentArray = contentArray.slice(findStartOfData(contentArray) - 2, findStartOfData(contentArray) + 1);
+    return contentArray;
+}
+function removeHeader(data) {
+    let contentArray = data.split('\n');
+    contentArray = contentArray.slice(findStartOfData(contentArray) + 1, contentArray.length - 2);
+    return contentArray;
+}
+function findStartOfData(data) {
+    let index = -1;
+    index = data.findIndex((element) => {
+        return element.includes("<PRE>");
+    });
+    return index + 3;
+}
+function displayData(data) {
+    const resultsDisplay = document.getElementById('results-display');
+    let headerArray = getDataHeader(data);
+    let dataArray = removeHeader(data);
+    for (let line of headerArray) {
+        resultsDisplay.value += line + '\n';
+    }
+    for (let line of dataArray) {
+        resultsDisplay.value += line + '\n';
+    }
+}
--- src/get-data.ts
+++ src/get-data.ts
@@ -1,3 +1,8 @@
+window.onload = () => {
+    (document.getElementById('results-display') as HTMLTextAreaElement).value = '';
+    (document.getElementById('date') as HTMLInputElement).valueAsDate = new Date();
+}
+
 const form = document.getElementById('search-form') as HTMLFormElement;
 
 form.addEventListener('submit', (event) => {
@@ -5,6 +10,9 @@ form.addEventListener('submit', (event) => {
 
     const date = (document.getElementById('date') as HTMLInputElement).value;
     const station = (document.getElementById('station') as HTMLInputElement).value;
+    const resultsDsiplay = (document.getElementById('results-display') as HTMLTextAreaElement);
+
+    resultsDsiplay.value = '';
 
     const queryParams = new URLSearchParams({
         "TYPE": "sflist", // Make it to be dynamic, maybe. UNITS as well.
@@ -16,10 +24,51 @@ form.addEventListener('submit', (event) => {
     fetch(`/getdata?${queryParams.toString()}`)
     .then(response => response.text())
     .then(data => {
-        (document.getElementById('results-display') as HTMLTextAreaElement).value = data;
+        const cleanedData = removeHeader(data);
+        displayData(data);
     })
     .catch(error => {
         console.error('Error fetching weather data: ', error);
         (document.getElementById('results-display') as HTMLTextAreaElement).value = 'Failed to load data.';
     })
 })
+
+function getDataHeader(data: string) {
+    let contentArray = data.split('\n');
+
+    contentArray = contentArray.slice(findStartOfData(contentArray) - 2, findStartOfData(contentArray) + 1);
+
+    return contentArray;
+}
+
+function removeHeader(data: string) {
+    let contentArray = data.split('\n');
+
+    contentArray = contentArray.slice(findStartOfData(contentArray) + 1, contentArray.length - 2);
+
+    return contentArray;
+}
+
+function findStartOfData(data: string[]) {
+    let index = -1;
+
+    index = data.findIndex((element) => {
+        return element.includes("<PRE>");
+    })
+
+    return index + 3;
+}
+
+function displayData(data: string) {
+    const resultsDisplay = (document.getElementById('results-display') as HTMLTextAreaElement);
+    let headerArray = getDataHeader(data);
+    let dataArray = removeHeader(data);
+
+    for (let line of headerArray) {
+        resultsDisplay.value += line + '\n';
+    }
+
+    for (let line of dataArray) {
+        resultsDisplay.value += line + '\n';
+    }
+}
