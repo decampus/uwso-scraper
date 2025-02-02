7ef13ecc5bf51083f99f2e7ea8c4ece13b3eab85
decampus <mos.decampus.cs@gmail.com>
2025-02-02 01:23:30 -0300

    feat: remove duplicates on days with missing records

--- public/get-data.js
+++ public/get-data.js
@@ -1,10 +1,15 @@
 import dayjs from 'dayjs';
 window.onload = () => {
     document.getElementById('results-display').value = '';
-    document.getElementById('start-date').valueAsDate = new Date();
-    document.getElementById('end-date').valueAsDate = new Date();
+    document.getElementById('start-date').valueAsDate = new Date('2025-01-15T00:00:00');
+    document.getElementById('end-date').valueAsDate = new Date('2025-01-19T00:00:00');
 };
 const form = document.getElementById('search-form');
+const fullHeader = [
+    "STN     TIME ALTM   TMP DEW RH  DIR SPD VIS  CLOUDS                  Weather     ",
+    "     DD/HHMM hPa    C   C   %   deg m/s km                                       ",
+    "==== ======= ====== === === === === === ==== ======= ======= ======= ============"
+];
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
     const startDate = document.getElementById('start-date').value;
@@ -13,34 +18,23 @@ form.addEventListener('submit', async (event) => {
     const resultsDisplay = document.getElementById('results-display');
     let duration = calculateTotalDaysBetweenDates(startDate, endDate);
     resultsDisplay.value = '';
-    if (duration > 1) {
+    if (duration >= 1) {
         loadingButton();
         let data = await getDataForDays(duration, startDate, station);
         loadingButtonDismiss();
-        displayData(data.header, data.data);
+        displayData(fullHeader, data);
     }
     else {
         loadingButton();
-        let rawData = await getData(endDate, station);
-        let header = getDataHeader(rawData);
-        let data = get24HoursRows(rawData);
+        let data = await getData(endDate, station);
         loadingButtonDismiss();
-        displayData(header, data);
+        displayData(fullHeader, data);
     }
 });
-function getDataHeader(data) {
-    let contentArray = data.split('\n');
-    contentArray = contentArray.slice(findStartOfData(contentArray) - 2, findStartOfData(contentArray) + 1);
-    return contentArray;
-}
-function get24HoursRows(data) {
-    let slicedData = removeHeader(data).slice(0, 24);
-    return slicedData;
-}
 function removeHeader(data) {
     let contentArray = data.split('\n');
     contentArray = contentArray.slice(findStartOfData(contentArray) + 1, contentArray.length - 2);
-    return contentArray;
+    return contentArray.slice(0, 24);
 }
 function findStartOfData(data) {
     let index = -1;
@@ -60,8 +54,7 @@ async function fetchData(queryParams) {
         if (!response.ok) {
             throw new Error(`HTTP error status: ${response.status}`);
         }
-        const data = await response.text();
-        return data;
+        return await response.text();
     }
     catch (error) {
         console.log('Error fetching weather data: ', error);
@@ -70,20 +63,28 @@ async function fetchData(queryParams) {
 }
 async function getDataForDays(duration, startDate, station) {
     let data = await getData(startDate, station);
-    let header = getDataHeader(data);
-    let content = removeHeader(data).slice(0, 24);
-    let final = content;
+    let final = data.slice(0, 24);
     let date = dayjs(startDate);
+    let previousDay = new Set();
+    let currentDay = new Set();
+    for (let line of data) {
+        previousDay.add(line);
+    }
     for (let i = 0; i < duration; i++) {
         date = date.add(1, 'day');
-        let newContent = await getData(date.toISOString().split('T')[0], station);
-        let newContentNoHeader = removeHeader(newContent).slice(0, 24);
-        final = final.concat(newContentNoHeader);
+        let newData = await getData(date.toISOString().split('T')[0], station);
+        for (let line of newData) {
+            currentDay.add(line);
+        }
+        let sameDayValues = setDifference(previousDay, currentDay);
+        previousDay.clear();
+        currentDay.forEach((day) => {
+            previousDay.add(day);
+        });
+        currentDay.clear();
+        final = final.concat([...sameDayValues]);
     }
-    return {
-        'header': header,
-        'data': final
-    };
+    return final;
 }
 async function getData(date, station) {
     const queryParams = new URLSearchParams({
@@ -94,11 +95,12 @@ async function getData(date, station) {
     });
     try {
         const result = await fetchData(queryParams);
-        return result || 'No data received.';
+        let data = removeHeader(result);
+        return data || 'No data received.';
     }
     catch (error) {
         console.error('Error: ', error);
-        return 'An unexpected error occurred.';
+        return ['An unexpected error occurred.'];
     }
 }
 function displayData(header, data) {
@@ -119,4 +121,13 @@ function loadingButtonDismiss() {
     const button = document.getElementById('submit-button');
     button.ariaBusy = 'false';
     button.textContent = 'Get results';
+}
+function setDifference(setA, setB) {
+    const result = new Set();
+    setB.forEach(value => {
+        if (!setA.has(value)) {
+            result.add(value);
+        }
+    });
+    return result;
 }
